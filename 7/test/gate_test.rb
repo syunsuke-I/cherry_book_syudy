21e679d035c4067adb93aa3d0bc043f5812a0fc8
syunsuke-I <odol@ymail.ne.jp>
2023-07-31 23:57:50 +0900

    7章 終わり

--- 7/test/gate_test.rb
+++ 7/test/gate_test.rb
@@ -4,21 +4,33 @@ require_relative '../lib/ticket'
 
 # テスト用
 class GateTest < Minitest::Test
-  # def test_gate
-  #   umeda = Gate.new(:umeda)
-  #   juso = Gate.new(:juso)
-  #   ticket = Ticket.new(160)
+  def setup
+    @umeda = Gate.new(:umeda)
+    @juso = Gate.new(:juso)
+    @mikuni = Gate.new(:mikuni)
+  end
 
-  #   umeda.enter(ticket)
-  #   assert juso.exit(ticket)
-  # end
+  def test_umeda_to_juso
+    ticket = Ticket.new(160)
+    @umeda.enter(ticket)
+    assert @juso.exit(ticket)
+  end
 
   def test_umeda_to_mikuni_when_fare_is_not_enough
-    umeda = Gate.new(:umeda)
-    mikuni = Gate.new(:mikuni)
     ticket = Ticket.new(160)
+    @umeda.enter(ticket)
+    refute @mikuni.exit(ticket)
+  end
 
-    umeda.enter(ticket)
-    refute mikuni.exit(ticket)
+  def test_umeda_to_mikuni_when_fare_is_enough
+    ticket = Ticket.new(190)
+    @umeda.enter(ticket)
+    assert @juso.exit(ticket)
+  end
+
+  def test_juso_to_mikuni
+    ticket = Ticket.new(160)
+    @juso.enter(ticket)
+    assert @mikuni.exit(ticket)
   end
 end
